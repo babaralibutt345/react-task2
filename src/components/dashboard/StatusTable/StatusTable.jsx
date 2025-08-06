6015ffe710743551c70766c4445446bda617c65b
babaralibutt345 <babar.zain316@gmail.com>
2025-08-07 02:17:40 +0500

    Financial Advisor Page

--- src/components/dashboard/StatusTable/StatusTable.jsx
+++ src/components/dashboard/StatusTable/StatusTable.jsx
@@ -9,20 +9,21 @@ const StatusTable = ({ title, headers, data }) => {
             case 'weak': return <span className="weak-badge">Weak</span>;
             case 'pending': return <span className="pending-badge">Pending</span>;
             case 'uploaded': return <span className="uploaded-badge">Uploaded</span>;
-            case 'generated': return <span className="generated-badge">Generated</span>;
+            case 'generated': return <span className="generated-badge">Generated</span>; 
+            case 'view profile': return <span className='profile-badge'>View Profile</span>
             default: return value;
         }
     };
 
 
     return (
-        <div className="card p-5 mt-4 mb-4 shadow-sm border-0">
+        <div className="card p-5 mt-3 mb-4 shadow-sm border-0">
             <div className="d-flex justify-content-between align-items-center pb-3 mb-2" style={{ borderBottom: '1px solid #D6E7FF' }}>
                 <h6>{title}</h6>
                 <a href="#" className='text-decoration-none view-all-btn'>View All</a>
             </div>
-            <div className="table-responsive status-table-wrapper">
-                <table className="table table-borderless m-0">
+            <div className="status-table-wrapper">
+                <table className=" table-borderless overflow-x-auto">
                     <colgroup>
                         {headers.map((_, i) => (
                             <col key={i} style={{ width: `${120 / headers.length}%` }} />
@@ -47,9 +48,9 @@ const StatusTable = ({ title, headers, data }) => {
                                 {row.map((cell, j) => (
                                     <td
                                         key={j}
-                                        className="px-1 py-3 tab-content"
+                                        className="px-1 py-3 tab-content text-nowrap"
                                     >
-                                        {typeof cell === 'string' && ['high', 'medium', 'weak', 'pending', 'uploaded', 'generated'].includes(cell.toLowerCase()) ? getBadge(cell) : cell}
+                                        {typeof cell === 'string' && ['high', 'medium', 'weak', 'pending', 'uploaded', 'generated', 'view profile'].includes(cell.toLowerCase()) ? getBadge(cell) : cell}
                                     </td>
                                 ))}
                             </tr>
--- src/pages/FinancialAdvisors/Index.jsx
+++ src/pages/FinancialAdvisors/Index.jsx
@@ -1,17 +1,34 @@
 import React from "react";
+import StatCard from "../../components/dashboard/StatCard/StatCard";
+import StatusTable from "../../components/dashboard/StatusTable/StatusTable";
 
-const FinancialAdvisorsIndex = () => {
+const Dashboard = () => {
+    const advisorData = [
+        ["Marguerite Nicolas", "Marguerite45@gmail.com", "$5,904.12", "58", "11-02-25", "12:40", "View Profile"],
+        ["Terry Upton", "Terry.Upton@gmail.com", "$65,735.69", "04", "22-02-25", "12:40", "View Profile"],
+        ["Marlene Kuvalis", "Marlene20@yahoo.com", "$852.45", "37", "04-03-25", "12:40", "View Profile"],
+        ["Zachary McLaughlin", "Zachary McLaughlin", "$8,226.98", "Nill", "14-03-25", "12:40", "View Profile"],
+        ["Lisa Effertz", "Lisa.Effertz73@gmail.com", "$32,646.74", "24", "17-03-25", "12:40", "View Profile"],
+        ["Garrett Nicolas", "Garrett90@gmail.com", "$32,646.74", "Nill", "22-03-25", "12:40", "View Profile"],
+        ["Lana Kerluke", "Lana_Kerluke@hotmail.com", "$32,646.74", "24", "17-03-25", "12:40", "View Profile"],
+        ["Blake Yost", "Blake_Yost@hotmail.com", "$32,646.74", "Nill", "22-03-25", "12:40", "View Profile"],
+        ["Ashley Dickens", "Ashley_Dickens@hotmail.com", "$32,646.74", "24", "17-03-25", "12:40", "View Profile"],
+        ["Arnold Waters", "Arnold.Waters76@hotmail.com", "$32,646.74", "Nill", "22-03-25", "12:40", "View Profile"],
+        ["Marcella Rippin", "Marcella.Rippin@yahoo.com", "$32,646.74", "Nill", "22-03-25", "12:40", "View Profile"],
+        ["Marcella Rippin", "Marcella.Rippin@yahoo.com", "$32,646.74", "Nill", "22-03-25", "12:40", "View Profile"]
+    ];
+
     return (
-        <div>
-            <h1>Financial Advisors Page</h1>
-        </div>
+        <>
+
+
+            <StatusTable
+                title="Financial Advisors"
+                headers={["Full Name", "Email", "Donations", "Total Clients", "Joining Date", "Last Login", "Action"]}
+                data={advisorData}
+            />
+        </>
     );
 };
 
-export default FinancialAdvisorsIndex;
-
-
-// // Inside pages/FinancialAdvisors/Index.jsx
-// export default function FinancialAdvisorsIndex() {
-//     return <h1>Financial Advisors Table</h1>;
-// }
+export default Dashboard;
